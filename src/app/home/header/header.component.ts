d919e0dcb87ed43d931017ad2e53e0f9342017ac
Danny Suárez <dalesupa22@gmail.com>
2019-03-29 18:18:58 -0500

    Ajuste redirect ver resultados

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalVideoService } from 'src/app/services/modal-video.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +9,17 @@ import { ModalVideoService } from 'src/app/services/modal-video.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private modalVideoService:ModalVideoService) { }
+  constructor(private modalVideoService:ModalVideoService,private router: Router) { }
 
   ngOnInit() {
   }
 
   abrirModal() {
     this.modalVideoService.abrirModal();
+  }
+
+  redirect(pagename: string) {
+    this.router.navigate(['/'+pagename]);
   }
 
 }
