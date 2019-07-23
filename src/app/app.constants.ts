062a2ec80cdc3cf26c53fce57ee29bb9ee513635
Danny Suarez <dalesupa22@gmail.com>
2019-07-23 09:59:08 -0400

    HTTPS added

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server = 'http://34.201.19.114:40003/';
+    public Server = 'https://34.201.19.114:40003/';
     public ApiUrl = '';
     public ServerWithApi = `${this.Server}${this.ApiUrl}`;
 }
