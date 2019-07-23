b160b4e5a209ca88f9a74b29edb9107e896cb28b
Danny Suarez <dalesupa22@gmail.com>
2019-07-23 10:07:05 -0400

    ssl help

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server = 'https://34.201.19.114:40003/';
+    //public Server = 'https://34.201.19.114:40003/';
+  public Server = 'https://back.oigame.org:40003/';
     public ApiUrl = '';
     public ServerWithApi = `${this.Server}${this.ApiUrl}`;
 }
