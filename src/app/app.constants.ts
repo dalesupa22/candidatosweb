d49094870ebe743b14cf66acfaa74a417c2501de
Danny Suarez <dalesupa22@gmail.com>
2020-06-18 22:53:35 -0500

    Url was changed

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Configuration {
     //public Server = 'https://34.201.19.114:40003/';
-  public Server = 'https://back.oigame.org:40003/';
+  public Server = 'https://testy.xertify.co:40003/';
     public ApiUrl = '';
     public ServerWithApi = `${this.Server}${this.ApiUrl}`;
 }
